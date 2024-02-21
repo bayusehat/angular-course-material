496de83960b3b6d6a84e6ddb9799a40d1975f821
Arya Bayu <aryabayu23@gmail.com>
2024-02-21 15:17:40 +0700

    databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,8 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
-  constructor(){}
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  constructor(){
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    }, 2000)
+  }
   ngOnInit(){
     
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
